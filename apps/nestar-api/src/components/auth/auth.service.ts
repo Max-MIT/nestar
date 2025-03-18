b58cb725173e087b24acde3321d0935c726dd6d7
Max-MIT <maksudbek198501@gmail.com>
2025-03-18 17:45:08 +0900

    feat: develop getMember GraphQL api

--- apps/nestar-api/src/components/auth/auth.service.ts
+++ apps/nestar-api/src/components/auth/auth.service.ts
@@ -3,7 +3,7 @@ import * as bcrypt from 'bcryptjs';
 import { Member } from '../../libs/dto/member/member';
 import { T } from '../../libs/types/common';
 import { JwtService } from '@nestjs/jwt';
-import { shapeIntoMongoObjecctId } from '../../libs/config';
+import { shapeIntoMongoObjectId } from '../../libs/config';
 
 @Injectable()
 export class AuthService {
@@ -30,7 +30,7 @@ export class AuthService {
 
     public async verifyToken(token: string): Promise<Member> {
         const member = await this.jwtService.verifyAsync(token);
-       member._id = shapeIntoMongoObjecctId(member._id);
+       member._id = shapeIntoMongoObjectId(member._id);
 		return member;
     }
 }
